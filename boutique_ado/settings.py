53456d677dbbb595d27e8ff90d058436e9c80302
Ciaran Griffin <griffinciaran3@gmail.com>
2024-09-05 09:27:57 +0000

    Added checkout app and models

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,10 +26,10 @@ SECRET_KEY = 'django-insecure-gmcq(7gi=o*e9^ymnii)6paamg$8tvz$(-_m6b8k+5n4ecg8f8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-boutiqueado-pyyqsrhwuz8.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ciarangriff-boutiqueado-p4x7tlefvj4.ws.codeinstitute-ide.net']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-boutiqueado-pyyqsrhwuz8.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-boutiqueado-p4x7tlefvj4.ws.codeinstitute-ide.net'
 ]
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
