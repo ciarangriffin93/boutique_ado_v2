9c528ab6852e96d88ed41fd263d1b3552297fe20
Ciaran Griffin <griffinciaran3@gmail.com>
2024-09-05 20:29:42 +0000

    Added env file

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -183,5 +186,5 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51PveJV2N9eXtrFSscCAKmEC7lJYxLgIPkxpUu1Homr6N7Y4PHsctUEokZRDU5kvICr7MZEAXy3N4dmj5qCcoQgte00lV2SIas7')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51PveJV2N9eXtrFSswP9xfXDTZmfa8L5wXsfLsQEiZ0Xt8mP4VC98Uz64VpPkcCXoSRWAbRsCi4bFBpdM1FIZyyxC00424N5leg')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'your-public-key')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'your-secret-key')
