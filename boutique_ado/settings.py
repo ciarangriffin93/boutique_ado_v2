0d7510cce7ec8a48c82db92e14f8419983425f44
Ciaran Griffin <griffinciaran3@gmail.com>
2024-09-09 14:07:25 +0000

    Fixed view on checkout

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -34,10 +34,10 @@ SECRET_KEY = 'django-insecure-gmcq(7gi=o*e9^ymnii)6paamg$8tvz$(-_m6b8k+5n4ecg8f8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ciarangriff-boutiqueado-uq7m5458677.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ciarangriff-boutiqueado-0zgbprn1ftl.ws.codeinstitute-ide.net']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ciarangriff-boutiqueado-uq7m5458677.ws.codeinstitute-ide.net'
+    'https://8000-ciarangriff-boutiqueado-0zgbprn1ftl.ws.codeinstitute-ide.net'
 ]
 
 # Application definition
