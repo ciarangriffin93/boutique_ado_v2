54c715b54aaf2041d78544fab834c3fa5732d417
Ciaran Griffin <griffinciaran3@gmail.com>
2024-09-13 10:44:44 +0000

    Created media

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -194,6 +194,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"  # where collected files will go
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
